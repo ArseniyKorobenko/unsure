4bf0787809dffc0a88263d51e83ee08a8c47f141
Arseniy Korobenko <72646905+ArseniyKorobenko@users.noreply.github.com>
2025-04-26 23:47:10 +0300

    add pert and uniform distributions

--- unsure.py
+++ unsure.py
@@ -1,62 +1,60 @@
+'''
+r(min, mode, max): PERT distribution (recommended), takes the mininum, most likely, and maximum value
+n(a, b): normal distribution, 90% confidence values are between `a` and `b`
+u(a, b): uniform distribution between `a` and `b`
+
+hist(np.array): convert from an array of random samples. size must match hist._samples to operate correctly
+distributions inherit np.ndarray, so you can use any numpy (exp, std, quantile, ...) and plotting operations on these objects.
+
+example usage:
+>>> r(1400,1500,1700) * r(0.55,0.60,0.65) - r(600,620,700) - r(100,120,200) - 30 - 20
+
+35.72~165.8  # (this is the 90% confidence window)
+
+100.0% |  above | ▒▒
+ 99.5% |  223.5 | ▒▒
+ 98.9% |  210.9 | ▒▒▒▒
+ 98.0% |  198.2 | ▒▒▒▒▒▒
+ 96.4% |  185.6 | ▒▒▒▒▒▒▒▒▒▒
+ 93.9% |  173   | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 90.3% |  160.4 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 85.4% |  147.7 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 79.3% |  135.1 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 71.6% |  122.5 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 62.9% |  109.8 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 53.5% |  97.22 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  (median = 99.02798633335368)
+ 43.7% |  84.59 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 34.1% |  71.96 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 25.4% |  59.33 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 17.9% |  46.71 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ 11.9% |  34.08 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  7.3% |  21.45 | ▒▒▒▒▒▒▒▒▒▒▒▒
+  4.2% |  8.823 | ▒▒▒▒▒▒▒▒
+  2.2% | -3.805 | ▒▒▒▒
+  1.1% | -16.43 | ▒▒
+  0.5% |  below | ▒▒
+
+>>> g = r(0,50,100)
+>>> (g>20) & (g<40)
+True  |  26.0% | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+False |  74.0% | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+for more information see Unsure Calculator https://filiph.github.io/unsure/
+''';
 import numpy as np
-class r(np.ndarray):
-    '''
-    r(a, b): 90% sure item is between a and b
-    r(np.array): convert from an array of random samples. size must match r._samples to interact with other ranges
-    inherits np.ndarray, so you can use any numpy operations (exp, quantile, std, ...)  on this object.
-
-    example usage:
-    >>> r(1400,1700) * r(0.55,0.65) - r(600,700) - r(100,200) - 30 - 20
-    
-    -56.76~220.8
-    
-    100.0% |  above | ▒▒▒▒▒▒
-     98.5% |  258   | ▒▒▒▒
-     97.5% |  239.7 | ▒▒▒▒▒▒
-     96.0% |  221.4 | ▒▒▒▒▒▒▒▒▒
-     93.9% |  203   | ▒▒▒▒▒▒▒▒▒▒▒▒▒
-     91.0% |  184.7 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     87.0% |  166.4 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     82.0% |  148   | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     75.8% |  129.7 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     68.7% |  111.4 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     60.6% |  93.03 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     52.1% |  74.69 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  (median = 79.42860413894547)
-     43.5% |  56.36 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     35.1% |  38.02 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     27.3% |  19.69 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     20.5% |  1.357 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     14.8% | -16.98 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     10.2% | -35.31 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-      6.8% | -53.64 | ▒▒▒▒▒▒▒▒▒▒▒
-      4.3% | -71.98 | ▒▒▒▒▒▒▒
-      2.6% | -90.31 | ▒▒▒▒
-      1.5% |  below | ▒▒▒▒▒▒
-
-    >>> g = r(0,100)
-    >>> (g>0) & (g<100)
-    True  |  90.0% | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-    False |  10.0% | ▒▒▒▒
-    
-    for more information see Unsure Calculator https://filiph.github.io/unsure/
-    '''
+class hist(np.ndarray):
+    '''Base class for distributions. plots a histogram'''
     _max_width = 40  # max bar width in characters
-    # _samples = 1_000_000
-    _samples =   250_000
-    def __new__(cls, a: float | np.ndarray, b: float | None = None, /):
-        if isinstance(a, np.ndarray):
-            data = a
-        else:
-            zscore = 2 * 1.64  # this determines the probability of values being between `a` and `b`
-            mean = (a + b) / 2
-            std = np.abs(a - b) / zscore
-            data = np.random.normal(mean, std, cls._samples)
-        return data.view(cls)
+    _samples   = 500_000
+
+    def __new__(cls, a, /):
+        return a.view(cls)
 
     def hist(self) -> str:
-        edge = 0.015
+        edge = 0.005
+        lines = 20
         a, median, b = np.quantile(self, [edge, 0.5, 1-edge])
-        counts, bin_edges = np.histogram(self, range=(a, b), bins=20, density=False)
+        counts, bin_edges = np.histogram(self, range=(a, b), bins=lines, density=False)
         counts = np.array([edge, *(counts / self.size), edge])  # sums to 1
         percentages = np.cumsum(counts)
         bin_midpoints = (bin_edges[:-1] + bin_edges[1:]) / 2
@@ -72,10 +70,40 @@ class r(np.ndarray):
     def __repr__(self): 
         if self.dtype == 'bool':
             t = self.sum() / self.size
-            f = 1-t
-            return f'True  | {t:6.1%} | {'▒' * int(self._max_width * t)}\n' +\
-                   f'False | {f:6.1%} | {'▒' * int(self._max_width * f)}'
-        q5, q95 = np.quantile(self, [0.05, 0.95])
-        if np.allclose(q5, q95):  # likely a single number
-            return repr((q5 + q95) / 2)
-        return f'{q5:.4g}~{q95:.4g}\n\n' + self.hist()
+            f = 1 - t
+            return f'True  | {t:6.1%} | {'▒' * int(self._max_width * t / max(t, f))}\n' +\
+                   f'False | {f:6.1%} | {'▒' * int(self._max_width * f / max(t, f))}'
+        a, b = np.quantile(self, [0.5, 0.95])
+        if np.allclose(a, b):  # likely a single number
+            return repr((a + b) / 2)
+        return f'{a:.4g}~{b:.4g}\n\n' + self.hist()
+
+
+class r(hist):
+    '''PERT distrubution'''
+    def __new__(cls, a, b, c, /):
+        if a > c: a, c = c, a
+        assert a <= b <= c
+        lamb = 4
+        r = c - a
+        alpha = 1 + lamb * (b - a) / r
+        beta = 1 + lamb * (c - b) / r
+        data = a + np.random.beta(alpha, beta, cls._samples) * r
+        return data.view(cls)
+
+
+class n(hist):
+    '''Normal distribution'''
+    def __new__(cls, a, b, /):
+        zscore = 2 * 1.64  # this determines the percentage of values between `a` and `b`
+        mean = (a + b) / 2
+        std = np.abs(a - b) / zscore
+        data = np.random.normal(mean, std, cls._samples)
+        return data.view(cls)
+
+
+class u(hist):
+    '''Uniform distribution'''
+    def __new__(cls, a, b, /):
+        data = np.random.uniform(min(a, b), max(a, b))
+        return data.view(cls)
