d0e614118054d75c6baa0dcf7ab9433891a3d6c4
Arseniy Korobenko <72646905+ArseniyKorobenko@users.noreply.github.com>
2025-05-02 12:27:25 +0300

    Update unsure.py

--- unsure.py
+++ unsure.py
@@ -1,46 +1,3 @@
-'''
-r(min, mode, max): PERT distribution (recommended), takes the mininum, most likely, and maximum value
-n(a, b): normal distribution, 90% confidence values are between `a` and `b`
-u(a, b): uniform distribution between `a` and `b`
-
-hist(np.array): convert from an array of random samples. size should match hist.samples to interact with other distrubutions
-distributions inherit np.ndarray, so you can use any array operations on these objects (np.exp, np.std, np.quantile, plt.hist, sns.kdeplot, etc)
-
-example usage:
->>> r(1400,1500,1700) * r(0.55,0.60,0.65) - r(600,620,700) - r(100,120,200) - 30 - 20
-
-35.72~165.8  # (this is the 90% confidence window)
-
-100.0% |  above | ▒▒
- 99.5% |  223.5 | ▒▒
- 98.9% |  210.9 | ▒▒▒▒
- 98.0% |  198.2 | ▒▒▒▒▒▒
- 96.4% |  185.6 | ▒▒▒▒▒▒▒▒▒▒
- 93.9% |  173   | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 90.3% |  160.4 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 85.4% |  147.7 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 79.3% |  135.1 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 71.6% |  122.5 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 62.9% |  109.8 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 53.5% |  97.22 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  (median = 99.02798633335368)
- 43.7% |  84.59 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 34.1% |  71.96 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 25.4% |  59.33 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 17.9% |  46.71 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- 11.9% |  34.08 | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-  7.3% |  21.45 | ▒▒▒▒▒▒▒▒▒▒▒▒
-  4.2% |  8.823 | ▒▒▒▒▒▒▒▒
-  2.2% | -3.805 | ▒▒▒▒
-  1.1% | -16.43 | ▒▒
-  0.5% |  below | ▒▒
-
->>> g = r(0,50,100)
->>> (g>20) & (g<40)
-True  |  26.0% | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-False |  74.0% | ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-
-for more information see Unsure Calculator https://filiph.github.io/unsure/
-''';
 import numpy as np
 class hist(np.ndarray):
     '''Base class for distributions. plots a histogram'''
@@ -50,11 +7,10 @@ class hist(np.ndarray):
     def __new__(cls, a, /):
         return a.view(cls)
 
-    def hist(self) -> str:
+    def hist(self, bins=20) -> str:
         edge = 0.005
-        lines = 20
         a, median, b = np.quantile(self, [edge, 0.5, 1-edge])
-        counts, bin_edges = np.histogram(self, range=(a, b), bins=lines, density=False)
+        counts, bin_edges = np.histogram(self, range=(a, b), bins=bins, density=False)
         counts = np.array([edge, *(counts / self.size), edge])  # sums to 1
         percentages = np.cumsum(counts)
         bin_midpoints = (bin_edges[:-1] + bin_edges[1:]) / 2
@@ -66,13 +22,16 @@ class hist(np.ndarray):
         lines[median_position] += f'  ({median = })'
         return '\n'.join(reversed(lines))
 
+    def bool_hist(self) -> str:
+        t = self.sum() / self.size
+        f = 1 - t
+        return f'True  | {t:6.1%} | {'▒' * int(self.max_width * t / max(t, f))}\n' +\
+               f'False | {f:6.1%} | {'▒' * int(self.max_width * f / max(t, f))}'
+
     def __str__(self): return repr(self)
     def __repr__(self): 
         if self.dtype == 'bool':
-            t = self.sum() / self.size
-            f = 1 - t
-            return f'True  | {t:6.1%} | {'▒' * int(self.max_width * t / max(t, f))}\n' +\
-                   f'False | {f:6.1%} | {'▒' * int(self.max_width * f / max(t, f))}'
+            return self.bool_hist()
         a, b = np.quantile(self, [0.5, 0.95])
         if np.allclose(a, b):  # likely a single number
             return repr((a + b) / 2)
